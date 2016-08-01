b311b1087ac66fed9376298988ce1ae7cdcbcbcd
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2016-08-01 14:26:51 +0200

    Create initial configuration

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+    name='flocs-core',
+    version='0.0.0.dev',
+    description='',
+    author='Tomas Effenberger',
+    author_email='tomas.effenberger@gmail.com',
+    packages=[
+        'flocs',
+    ],
+    install_requires=[
+    ],
+    license='MIT',
+)
