464e4eb9803889c94bdd4a8ccf55143fc51c0033
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2016-08-31 15:10:04 +0200

    Test actions

--- flocs/tests/test_actions.py
+++ flocs/tests/test_actions.py
@@ -28,11 +28,44 @@ def test_create_student_at_state():
 
 
 def test_create_student_without_id(monkeypatch):
-    monkeypatch.setattr('flocs.context.uuid4', lambda: 974375)
+    monkeypatch.setattr('flocs.context.uuid4', lambda: 974)
     action = actions.create_student()
     expected_action = Action(
         type='create_student',
-        data={'student_id': 974375},
+        data={'student_id': 974},
+        context=None,
+        meta=META,
+    )
+    assert action == expected_action
+
+
+def test_start_task():
+    action = actions.start_task(student_id=145, task_id=782, task_instance_id=381)
+    expected_action = Action(
+        type='start_task',
+        data={'student_id': 145, 'task_id': 782, 'task_instance_id': 381},
+        context=None,
+        meta=META,
+    )
+    assert action == expected_action
+
+
+def test_solve_task():
+    action = actions.solve_task(task_instance_id=651)
+    expected_action = Action(
+        type='solve_task',
+        data={'task_instance_id': 651},
+        context=None,
+        meta=META,
+    )
+    assert action == expected_action
+
+
+def test_give_up_task():
+    action = actions.give_up_task(task_instance_id=798)
+    expected_action = Action(
+        type='give_up_task',
+        data={'task_instance_id': 798},
         context=None,
         meta=META,
     )
