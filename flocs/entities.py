607f7a8c7657ae4ac324decf55f39f2cec21360a
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2016-09-05 10:16:04 +0200

    Add a few entities

--- flocs/entities.py
+++ flocs/entities.py
@@ -5,13 +5,13 @@ from collections import namedtuple
 
 Student = namedtuple('Student', [
     'student_id',
-    ])
+])
 
 
 Task = namedtuple('Task', [
     'task_id',
     'ref',
-    ])
+])
 
 
 TaskInstance = namedtuple('TaskInstance', [
@@ -20,7 +20,7 @@ TaskInstance = namedtuple('TaskInstance', [
     'task_id',
     'solved',
     'given_up',
-    ])
+])
 
 
 TaskStats = namedtuple('TaskStats', [
@@ -28,4 +28,45 @@ TaskStats = namedtuple('TaskStats', [
     'started_count',
     'solved_count',
     'given_up_count',
-    ])
+])
+
+
+Concept = namedtuple('Concept', [
+    'concept_id',
+    'ref',
+    'type',
+])
+
+
+Toolbox = namedtuple('Toolbox', [
+    'toolbox_id',
+    'ref',
+    'blocks_ids',
+])
+
+
+Block = namedtuple('Block', [
+    'block_id',
+    'ref',
+])
+
+
+Instruction = namedtuple('Instruction', [
+    'instruction_id',
+    'concept_id',
+    'ref',
+])
+
+
+ActivityRecommendation = namedtuple('ActivityRecommendation', [
+    'activity_recommendation_id',
+    'student_id',
+    'activity',
+])
+
+
+TaskRecommendation = namedtuple('TaskRecommendation', [
+    'task_recommendation_id',
+    'student_id',
+    'task_id',
+])
